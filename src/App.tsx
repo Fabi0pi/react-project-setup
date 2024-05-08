6dc0186db421d474d569123de0a137032e7ef655
Fabio Piras <fabio.piras@spindox.it>
2024-05-08 19:32:50 +0200

    fix: linter rules

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,8 @@ import './app.css'
 
 function App() {
   return (
-    <div className="app-container">
-      <span>YOUR</span>
-      <span>REACT</span>
-      <span>APP</span>
-      <span>HERE</span>
-    </div>
+    <>
+    </>
   )
 }
 
